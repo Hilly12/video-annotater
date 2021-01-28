4f5140f32f4fe601075b169bc7ac260b6c69842a
Aahil <aahilmehta12@gmail.com>
2021-01-28 19:28:50 +0000

    complete task

---
+++ src/assets/VideoData.js
@@ -0,0 +1,87 @@
+import rickrollst from "./rickrollst.srt";
+
+const elephantsDream = {
+  src: "https://thepaciellogroup.github.io/AT-browser-tests/video/ElephantsDream.mp4",
+  subtitles: "https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt",
+  description: `Friends Proog and Emo journey inside the folds of a seemingly infinite Machine, exploring the dark and
+   twisted complex of wires, gears, and cogs, until a moment of conflict negates all their assumptions.`
+}
+
+const elephantsDreamAligned = {
+  src: "https://thepaciellogroup.github.io/AT-browser-tests/video/ElephantsDream.mp4",
+  subtitles: "https://thepaciellogroup.github.io/AT-browser-tests/video/subtitles-en.vtt",
+  description: `Friends Proog and Emo journey inside the folds of a seemingly infinite Machine, exploring the dark and
+   twisted complex of wires, gears, and cogs, until a moment of conflict negates all their assumptions.`,
+  alignedWords: [
+    { "word": "Emo", "start": 2.17, "end": 2.6615 },
+    { "word": "close", "start": 2.6615, "end": 3.153 },
+    { "word": "your", "start": 3.153, "end": 3.6445 },
+    { "word": "eyes", "start": 3.6445, "end": 4.136 },
+    { "word": "Why", "start": 4.136, "end": 4.8665 },
+    { "word": "NOW", "start": 4.8665, "end": 5.597 },
+    { "word": "Ok", "start": 5.597, "end": 7.405 },
+    { "word": "Good", "start": 7.405, "end": 8.803 },
+    { "word": "What", "start": 8.803, "end": 9.107222222222223 },
+    { "word": "do", "start": 9.107222222222223, "end": 9.411444444444445 },
+    { "word": "you", "start": 9.411444444444445, "end": 9.715666666666667 },
+    { "word": "see", "start": 9.715666666666667, "end": 10.01988888888889 },
+    { "word": "at", "start": 10.01988888888889, "end": 10.324111111111112 },
+    { "word": "your", "start": 10.324111111111112, "end": 10.628333333333334 },
+    { "word": "left", "start": 10.628333333333334, "end": 10.932555555555556 },
+    { "word": "side", "start": 10.932555555555556, "end": 11.236777777777778 },
+    { "word": "Emo", "start": 11.236777777777778, "end": 11.541 },
+    { "word": "Well", "start": 11.541, "end": 13.287 },
+    { "word": "Er", "start": 13.287, "end": 14.228 },
+    { "word": "nothing", "start": 14.228, "end": 15.169 },
+    { "word": "Really", "start": 15.169, "end": 16.11 },
+    { "word": "No", "start": 16.11, "end": 16.711 },
+    { "word": "nothing", "start": 16.711, "end": 17.311999999999998 },
+    { "word": "at", "start": 17.311999999999998, "end": 17.913 },
+    { "word": "all", "start": 17.913, "end": 18.514 },
+    { "word": "Really", "start": 18.514, "end": 18.810785714285714 },
+    { "word": "and", "start": 18.810785714285714, "end": 19.10757142857143 },
+    { "word": "at", "start": 19.10757142857143, "end": 19.404357142857144 },
+    { "word": "your", "start": 19.404357142857144, "end": 19.701142857142855 },
+    { "word": "right", "start": 19.701142857142855, "end": 19.99792857142857 },
+    { "word": "What", "start": 19.99792857142857, "end": 20.294714285714285 },
+    { "word": "do", "start": 20.294714285714285, "end": 20.5915 },
+    { "word": "you", "start": 20.5915, "end": 20.888285714285715 },
+    { "word": "see", "start": 20.888285714285715, "end": 21.18507142857143 },
+    { "word": "at", "start": 21.18507142857143, "end": 21.481857142857145 },
+    { "word": "your", "start": 21.481857142857145, "end": 21.778642857142856 },
+    { "word": "right", "start": 21.778642857142856, "end": 22.07542857142857 },
+    { "word": "side", "start": 22.07542857142857, "end": 22.372214285714286 },
+    { "word": "Emo", "start": 22.372214285714286, "end": 22.669 },
+    { "word": "Umm", "start": 22.669, "end": 23.5295 },
+    { "word": "the", "start": 23.5295, "end": 24.39 },
+    { "word": "same", "start": 24.39, "end": 25.250500000000002 },
+    { "word": "Proog", "start": 25.250500000000002, "end": 26.111 },
+    { "word": "Exactly", "start": 26.111, "end": 26.74475 },
+    { "word": "the", "start": 26.74475, "end": 27.378500000000003 },
+    { "word": "same", "start": 27.378500000000003, "end": 28.01225 },
+    { "word": "Nothing", "start": 28.01225, "end": 28.646 },
+    { "word": "Great", "start": 28.646, "end": 30.794 }
+  ]
+}
+
+const rickroll = {
+  src: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  subtitles: rickrollst,
+  description: `We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of 
+            You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand 
+            Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make 
+            you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long 
+            Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know 
+            the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see 
+            Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make 
+            you cry Never gonna say goodbye Never gonna tell a lie and hurt you No, I'm never gonna give you up No, 
+            I'm never gonna let you down No, I'll never run around and hurt you Never, ever desert you We've known 
+            each other for so long Your heart's been aching but Never gonna give you up Never gonna let you down 
+            Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell 
+            a lie and hurt you No, I'm never gonna give you up No, I'm never gonna let you down No, I'll never run 
+            around and hurt you I'll never, ever desert you`
+}
+
+const video = {
+  rickroll: rickroll
+}
